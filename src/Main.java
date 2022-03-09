31d7a1bf7cf63c23ff05d27489a255ec8882dec4
jnior-B <jniorbamba@yahoo.fr>
2022-03-09 00:24:58 -0300

    Final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -25,9 +27,45 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+        */
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developper");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developper");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devJunior =new Dev();
+        devJunior.setNome("Junior");
+        devJunior.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Junior" + devJunior.getConteudosInscritos());
+        devJunior.progredir();
+        devJunior.progredir();
+        devJunior.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Junior" + devJunior.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Junior" + devJunior.getConteudosConcluidos());
+        System.out.println("XP:" + devJunior.calcularTotalXp());
+
+
+        System.out.println("-------------");
+
+
+        Dev devBamba = new Dev();
+        devBamba.setNome("Bamba");
+        devBamba.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Bamba" + devBamba.getConteudosInscritos());
+        devBamba.progredir();
+        devBamba.progredir();
+        devBamba.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscritos Bamba" + devBamba.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Bamba" + devBamba.getConteudosConcluidos());
+        System.out.println("XP:" + devBamba.calcularTotalXp());
 
     }
 }
